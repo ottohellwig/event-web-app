7223f4f2a3773a00c3e91cbc552fb5170cfc6557
Otto Hellwig <105997582+ottohellwig@users.noreply.github.com>
2023-08-31 12:31:42 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from website import create_app
 
 if __name__=='__main__':
-    app=create_app()
-    app.run(debug=True)
-    #disable debug mode once successfully deployed
+    n_app=create_app()
+    n_app.run(debug=True)
